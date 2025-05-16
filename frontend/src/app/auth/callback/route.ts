de68dad6e5ca9e8d17a68ff8a6718a11bf6c58cc
BereketMelese <bereketmeles00@gmail.com>
2025-05-16 14:12:17 +0300

    Temp: save callback route changes

--- frontend/src/app/auth/callback/route.ts
+++ frontend/src/app/auth/callback/route.ts
@@ -1,5 +1,5 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import {cookies} from 'next/headers'
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
@@ -14,5 +14,5 @@ export async function GET(request: NextRequest) {
   }
 
   // URL to redirect to after sign in process completes
-   return NextResponse.redirect(new URL("/dashboard", request.url))
+  return NextResponse.redirect(new URL("/", request.url));
 }
